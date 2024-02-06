5ad2bdeda792b0440f74886c517e627dfa3934ca
PallonCX <cheexiang03@gmail.com>
2024-02-06 21:28:52 +0800

    Create component folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+// App.js
+
 import './App.css';
+import Homepage from './component/Homepage'; // Correcting the path to Homepage.js
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Homepage /> {/* Render the Homepage component */}
       </header>
     </div>
   );
---
+++ src/component/Homepage.js
@@ -0,0 +1,15 @@
+// Homepage.js
+
+import React from 'react';
+import './Homepage.css';
+
+function Homepage() {
+  return (
+    <div className="Homepage-container">
+      <h1 className="Homepage-title">Welcome to the Homepage!</h1>
+      <p className="Homepage-content">This is the homepage content!</p>
+    </div>
+  );
+}
+
+export default Homepage;
